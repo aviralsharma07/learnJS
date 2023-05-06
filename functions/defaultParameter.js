d748f4287f7102c4810b4e48eb9939e9f3b01619
aviralsharma07 <thisisavi200@gmail.com>
2023-05-06 11:32:23 +0530

    learn default parameter

---
+++ functions/defaultParameter.js
@@ -0,0 +1,14 @@
+const ha = function(){
+    console.log("Hello Avi :)");
+}
+ha();
+
+
+console.log(add2Num(19));
+function add2Num(a = 0, b = 0){
+    return a+b;
+}
+
+
+const multiply2Num = (a,b=1) => {return a*b};
+console.log(multiply2Num(7,5));
