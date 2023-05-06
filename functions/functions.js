53a1f4ddfa2cedfccdb444fc4b003b8c0ff8ab67
aviralsharma07 <thisisavi200@gmail.com>
2023-05-06 11:08:53 +0530

    learn function basics

---
+++ functions/functions.js
@@ -0,0 +1,22 @@
+function helloWorld (){
+    console.log("Normal Function");
+}
+helloWorld();
+
+
+// FUNCTION EXPRESSION
+const helloWorld1 = function (){
+    console.log("Function Expression");
+}
+helloWorld1();
+
+
+// ARROW FUNCTIONS - Function keyword is removed
+const isEven = (number) => {
+    return (number%2 === 0);
+}
+console.log(isEven(13));
+
+
+const isOdd = number => number%2 !== 0;
+console.log(isOdd(19));
