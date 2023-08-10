92b3549f0ea4947896bbc4355c6411a5661505af
aviralsharma07 <thisisavi200@gmail.com>
2023-08-10 19:10:40 +0530

    Add More Useful Topics

---
+++ moreUsefulThings/optionalChaining.js
@@ -0,0 +1,8 @@
+const user = {
+    firstName: "Nakesh",
+    // address: {houseNumber: 24}
+};
+
+console.log(user.firstName);
+console.log(user.address?.houseNumber);
+// Question Mark Checks if the Object exist and access its property only if does. 
